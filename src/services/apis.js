ce99a5b5e8eba48c26a37f7d441cc895692f607c
Alok Deep <alokdeep9925@gmail.com>
2024-06-08 16:04:37 +0530

    adding new backend URL

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://astrowala-backend-deployed.onrender.com";
+const BASE_URL = "https://astrowala-backend-deployed.onrender.com/api/v1";
 
 // AUTH ENDPOINTS
 export const endpoints = {
